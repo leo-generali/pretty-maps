c77ae6fbcb85f716b3500964e7ae52c0db2f65d9
Leo Generali <leogen17@gmail.com>
2019-02-02 12:23:23 -0500

    Make map image appear rather than mapbox

--- assets/javascript/activity/index.js
+++ assets/javascript/activity/index.js
@@ -20,7 +20,6 @@ const run = () => {
     container: 'map-container',
     style: 'mapbox://styles/leogenerali/cjrmbdavm16fr2srwjg66llb1',
     center: center,
-    attributionControl: false,
     interactive: false,
     zoom: 12,
     preserveDrawingBuffer: true
@@ -31,6 +30,11 @@ const run = () => {
     map.fitBounds([[bounds.minX, bounds.minY], [bounds.maxX, bounds.maxY]], {
       padding: { top: 50, bottom: 50, left: 50, right: 50 }
     });
+  });
+
+  map.once('idle', () => {
+    const imageURI = map.getCanvas().toDataURL('image/png');
+    document.querySelector('#map-image').setAttribute('src', imageURI);
   });
 
   document.querySelector('#downloadLink').addEventListener('click', function() {
