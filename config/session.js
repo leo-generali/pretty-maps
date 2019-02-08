887bbec0f8ad75c4a4f00b740c9d7a1dbee725d0
Leo Generali <leogen17@gmail.com>
2019-02-08 11:18:12 -0500

    Add cookie parser

--- config/session.js
+++ config/session.js
@@ -2,7 +2,6 @@ const session = require('express-session');
 const FileStore = require('session-file-store')(session);
 
 const sessionConfig = session({
-  // store: new FileStore(),
   secret: 'pretty-strava-maps',
   resave: false,
   saveUninitialized: false
