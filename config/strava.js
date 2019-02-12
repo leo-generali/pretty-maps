f812c82535a3d5c403385fed168075fb1a5cdb0b
Leo Generali <leogen17@gmail.com>
2019-02-12 10:09:49 -0500

    Add different enviroment variables for heroku

--- config/strava.js
+++ config/strava.js
@@ -1,10 +1,11 @@
 const STRAVA_CLIENT_ID = process.env.STRAVA_CLIENT_ID;
 const STRAVA_CLIENT_SECRET = process.env.STRAVA_CLIENT_SECRET;
+const STRAVA_CALLBACK = process.env.STRAVA_CALLBACK;
 
 const stravaConfig = {
   clientID: STRAVA_CLIENT_ID,
   clientSecret: STRAVA_CLIENT_SECRET,
-  callbackURL: 'http://127.0.0.1:3000/auth/strava/callback'
+  callbackURL: STRAVA_CALLBACK
 };
 
 module.exports = stravaConfig;
