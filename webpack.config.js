a555de85e202df9f3eb8874fb1852bc108ace25a
Leo Generali <leogen17@gmail.com>
2019-02-15 11:59:07 -0500

    Update webpack config to reflect updates to new organization

--- webpack.config.js
+++ webpack.config.js
@@ -58,8 +58,8 @@ const styles = {
 const config = {
   entry: {
     // we only have 1 entry, but I've set it up for multiple in the future
-    App: './assets/webpack-entry.js',
-    MapEditor: './assets/map-editor.js'
+    App: './app/assets/webpack-entry.js',
+    MapEditor: './app/assets/map-editor.js'
   },
   // we're using sourcemaps and here is where we specify which kind of sourcemap to use
   devtool: 'source-map',
