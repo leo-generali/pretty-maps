0580d1bab23ce369d1300fcda7a7c50d4fdb330e
Leo Generali <leogen17@gmail.com>
2019-02-15 12:38:40 -0500

    get pjax working with app. tbd if it matters

--- app/assets/webpack-entry.js
+++ app/assets/webpack-entry.js
@@ -1,1 +1,15 @@
+import Pjax from 'pjax';
+
 import './stylesheets/style.scss';
+
+const pjax = new Pjax({
+  elements: ['a.navbar__link'],
+  selectors: [
+    'title',
+    'meta[name=description]',
+    '.flash-message-wrapper',
+    '.navbar',
+    '.main'
+  ],
+  cacheBust: false
+});
